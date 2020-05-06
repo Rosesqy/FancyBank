b02f0dd494045699096b5ef4dd48d990c781aab6
Qingyang Shi <Rosesqy@users.noreply.github.com>
2020-05-05 22:13:07 -0400

    customerpwdencrypt

--- Bank/Utilities/DbHelperPSQL.java
+++ Bank/Utilities/DbHelperPSQL.java
@@ -1,53 +1,51 @@
 package Bank.Utilities;
 
 import Bank.DAO.*;
+import Bank.Utilities.*;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.sql.*;
 
 public class DbHelperPSQL {
-   // static String url = "jdbc:postgresql://localhost:5432/FancyBank";
-   // static String username = "postgres";
-   // static String pwd = "123456";
    String url;
    String username;
    String pwd;
    Connection c;
    String psql;
+   DbConnectionHelper dbcon;
+   PasswordEncryptionService pwdencrypt;
 
-   public DbHelperPSQL(){
+   public DbHelperPSQL() {
       this.url = "jdbc:postgresql://localhost:5432/fancybank";
       this.username = "postgres";
       this.pwd = null;
+      this.test();
    }
 
    public DbHelperPSQL(String url, String username, String pwd) {
       this.url = url;
       this.username = username;
       this.pwd = pwd;
+      this.test();
       
-      if (!test()) {
-         initialize();
-      }
    }
 
-   public boolean test() {
-      // check if need to initialize tables in the database based on the existence of customer table
+   public void test() {
+      // check if need to initialize tables in the database based on the existence of
+      // customer table
       c = this.getConnection();
       DatabaseMetaData md;
       try {
          md = c.getMetaData();
          ResultSet rs = md.getTables(null, null, "customer", null);
-         Statement stmt = c.createStatement();
-         //drop the original tableS first
-         // stmt.execute("DROP SCHEMA PUBLIC CASCADE");
-         // stmt.execute("CREATE SCHEMA PUBLIC");
-         // System.out.println("schema restarted");
-         // System.out.println("rs.next() "+rs.next());
-         return rs.next();
+         if (!rs.next())
+            new InitializeTables();
+
       } catch (SQLException e) {
          e.printStackTrace();
       }
-      return false;
+
    }
 
    public Connection getConnection() {
@@ -56,60 +54,16 @@ public class DbHelperPSQL {
          Class.forName("org.postgresql.Driver");
          c = DriverManager.getConnection(this.url, this.username, this.pwd);
          c.setAutoCommit(false);
+         System.out.println("Opened database successfully");
       } catch (Exception e) {
          e.printStackTrace();
          System.err.println(e.getClass().getName() + ": " + e.getMessage());
          System.exit(0);
       }
-      System.out.println("Opened database successfully");
+      // dbcon = new DbConnectionHelper();
+      // c = dbcon.getConnection();
+      // System.out.println("Opened database successfully");
       return c;
-   }
-
-   public void initialize() {
-      String psql = "CREATE TABLE CUSTOMER " + "(ID BIGSERIAL PRIMARY KEY NOT NULL," + "USERNAME TEXT NOT NULL UNIQUE,"
-            + "FIRST_NAME TEXT NOT NULL," + "MIDDLE_NAME TEXT," + "LAST_NAME TEXT NOT NULL,"
-            + "PHONE_NUM TEXT NOT NULL," + "EMAIL TEXT NOT NULL,"+"PWD TEXT NOT NULL)";
-      this.createTable("customer", psql);
-
-      psql = "CREATE TABLE SAVING_ACC " + "(SAVINGID BIGSERIAL PRIMARY KEY NOT NULL," + "CUSTOMERID BIGINT REFERENCES CUSTOMER(ID) NOT NULL,"
-            + "BALANCE MONEY NOT NULL," +"CURRENCY VARCHAR(5) NOT NULL,"
-            + "INTEREST_RATE NUMERIC(8,4) NOT NULL," + "CLOSEFEE MONEY NOT NULL,"
-            + "OPENFEE MONEY NOT NULL)";
-      this.createTable("saving_acc", psql);
-
-      psql = "CREATE TABLE CHECKING_ACC " + "(CHECKINGID BIGSERIAL PRIMARY KEY NOT NULL,"
-            + "CUSTOMERID BIGINT REFERENCES CUSTOMER(ID) NOT NULL," + "BALANCE MONEY NOT NULL," +"CURRENCY VARCHAR(5) NOT NULL,"
-            + "CLOSEFEE MONEY NOT NULL," + "OPENFEE MONEY NOT NULL)";
-      this.createTable("checking_acc", psql);
-
-      psql = "CREATE TABLE SEC_ACC " + "(SECID BIGSERIAL PRIMARY KEY NOT NULL," + "CUSTOMERID BIGINT REFERENCES CUSTOMER(ID) NOT NULL,"
-            + "BALANCE MONEY NOT NULL," + "CURRENCY VARCHAR(5) NOT NULL,"
-            +"CLOSEFEE MONEY NOT NULL," + "OPENFEE MONEY NOT NULL)";
-      this.createTable("sec_acc", psql);
-
-      psql = "CREATE TABLE TRANSACTION " + "(TRANSID BIGSERIAL PRIMARY KEY NOT NULL," + "FROMNAME TEXT NOT NULL,"
-            + "FROMID BIGINT NOT NULL," + "TONAME TEXT NOT NULL," + "TOID BIGINT NOT NULL," + "AMT MONEY NOT NULL,"
-            + "CURRENCY VARCHAR(5) NOT NULL," + "TIME TIMESTAMP NOT NULL)";
-      this.createTable("transaction", psql);
-
-      psql = "CREATE TABLE LOAN " + "(LOANID BIGSERIAL PRIMARY KEY NOT NULL," + "CUSTOMERNAME TEXT NOT NULL,"
-            + "CUSTOMERID BIGINT NOT NULL," + "AMT MONEY NOT NULL," + "COLLATERAL TEXT NOT NULL,"
-            + "COLLATERAL_VALUE MONEY NOT NULL," + "INTEREST_RATE NUMERIC(8,4) NOT NULL," + "TIME TIMESTAMP NOT NULL)";
-      this.createTable("loan", psql);
-
-      psql = "CREATE TABLE STOCKMKT " + "(STOCKID BIGSERIAL PRIMARY KEY NOT NULL," + "STOCKNAME TEXT NOT NULL,"
-            + "QUANTITY BIGINT NOT NULL," + "PRICE MONEY NOT NULL)";
-      this.createTable("stockmkt", psql);
-
-      psql = "CREATE TABLE MYSTOCK " + "(STOCKID BIGSERIAL PRIMARY KEY NOT NULL," + "STOCKNAME TEXT NOT NULL,"
-            + "CUSTOMERNAME TEXT NOT NULL," + "CUSTOMERSECID BIGINT NOT NULL," + "PURCHASE_PRICE MONEY NOT NULL,"
-            + "PURCHASE_QUANTITY BIGINT NOT NULL," + "CURRENT_PRICE MONEY NOT NULL)";
-      this.createTable("mystock", psql);
-
-      psql = "CREATE TABLE STOCK_TRANS " + "(STRANSID BIGSERIAL PRIMARY KEY," + "STOCKNAME TEXT NOT NULL,"
-            + "CUSTOMERNAME TEXT NOT NULL," + "CUSTOMERSECID BIGINT NOT NULL," + "PRICE MONEY NOT NULL,"
-            + "QUANTITY BIGINT NOT NULL," + "TIME TIMESTAMP NOT NULL)";
-      this.createTable("stock_trans", psql);
    }
 
    public void alterTable(String tabname, String psql) {
@@ -120,7 +74,6 @@ public class DbHelperPSQL {
       Connection c = this.getConnection();
       try {
          Statement stmt = c.createStatement();
-
          stmt.executeUpdate(psql);
          System.out.println(tabname + " table created");
          stmt.close();
@@ -131,19 +84,31 @@ public class DbHelperPSQL {
       }
    }
 
-   public boolean insertCustomer(Customer customer) {
+   public boolean insertCustomer(String uname, String fname, String mname, String lname, String phone, String email,
+         String pwd) {
       Connection c = this.getConnection();
-      String psql = "INSERT INTO CUSTOMER(USERNAME, FIRST_NAME, MIDDLE_NAME,LAST_NAME, PHONE_NUM, EMAIL, PWD)"
-            + "VALUES(?,?,?,?,?,?,?)";
+      String psql = "INSERT INTO CUSTOMER(USERNAME, FIRST_NAME, MIDDLE_NAME,LAST_NAME, PHONE_NUM, EMAIL, SALT, PWD)"
+            + "VALUES(?,?,?,?,?,?,?,?)";
       try {
+         //produce salt and encrypted password
+         pwdencrypt = new PasswordEncryptionService();
+         byte[] salt = null;
+         byte[] epwd = null;
+         try {
+            salt = pwdencrypt.generateSalt();
+            epwd = pwdencrypt.getEncryptedPassword(pwd, salt);
+         } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+         }
          PreparedStatement pstmt = c.prepareStatement(psql, Statement.RETURN_GENERATED_KEYS);
-         pstmt.setString(1, customer.getUname());
-         pstmt.setString(2, customer.getFname());
-         pstmt.setString(3, customer.getMname());
-         pstmt.setString(4, customer.getLname());
-         pstmt.setString(5, customer.getPhone());
-         pstmt.setString(6, customer.getEmail());
-         pstmt.setString(7, customer.getPwd());
+         pstmt.setString(1, uname);
+         pstmt.setString(2, fname);
+         pstmt.setString(3, mname);
+         pstmt.setString(4, lname);
+         pstmt.setString(5, phone);
+         pstmt.setString(6, email);
+         pstmt.setBytes(7, salt);
+         pstmt.setBytes(8, epwd);
          int result = pstmt.executeUpdate();
          System.out.println(result+" customer created");
          pstmt.close();
@@ -156,18 +121,18 @@ public class DbHelperPSQL {
       return false;
    }
 
-   public boolean insertSavAccount(Savings savacc) {
+   public boolean insertSavAccount(String cid, double balance, String cursig, double interest, double openfee, double closefee) {
       Connection c = this.getConnection();
       String psql = "INSERT INTO SAVING_ACC(CUSTOMERID, BALANCE, CURRENCY, INTERESTRATE,OPENFEE, CLOSEFEE)"
             + "VALUES(?,?,?,?,?,?)";
       try {
          PreparedStatement pstmt = c.prepareStatement(psql, Statement.RETURN_GENERATED_KEYS);
-         pstmt.setString(1, savacc.getCustomerID());
-         pstmt.setDouble(2, savacc.getBalance());
-         pstmt.setString(3, savacc.getCurSig());
-         pstmt.setDouble(4, savacc.getInterest());
-         pstmt.setDouble(5, savacc.getOpenFee());
-         pstmt.setDouble(6, savacc.getCloseFee());
+         pstmt.setString(1, cid);
+         pstmt.setDouble(2, balance);
+         pstmt.setString(3, cursig);
+         pstmt.setDouble(4, interest);
+         pstmt.setDouble(5, openfee);
+         pstmt.setDouble(6, closefee);
          int result = pstmt.executeUpdate();
          System.out.println(result+" saveacc created");
          pstmt.close();
@@ -180,17 +145,17 @@ public class DbHelperPSQL {
       return false;
    }
 
-   public boolean insertCheckAccount(Checking checkacc) {
+   public boolean insertCheckAccount(String cid, double balance, String cursig, double openfee, double closefee) {
       Connection c = this.getConnection();
       String psql = "INSERT INTO CECKING_ACC(CUSTOMERID, BALANCE, CURRENCY, OPENFEE, CLOSEFEE)"
             + "VALUES(?,?,?,?,?)";
       try {
          PreparedStatement pstmt = c.prepareStatement(psql, Statement.RETURN_GENERATED_KEYS);
-         pstmt.setString(1, checkacc.getCustomerID());
-         pstmt.setDouble(2, checkacc.getBalance());
-         pstmt.setString(3, checkacc.getCurSig());
-         pstmt.setDouble(4, checkacc.getOpenFee());
-         pstmt.setDouble(5, checkacc.getCloseFee());
+         pstmt.setString(1, cid);
+         pstmt.setDouble(2, balance);
+         pstmt.setString(3, cursig);
+         pstmt.setDouble(4, openfee);
+         pstmt.setDouble(5, closefee);
          int result = pstmt.executeUpdate();
          System.out.println(result + " checking account created"); 
          pstmt.close();
@@ -203,17 +168,17 @@ public class DbHelperPSQL {
       return false;
    }
 
-   public boolean insertSecAccount(Security secacc) {
+   public boolean insertSecAccount(String cid, double balance, String cursig, double openfee, double closefee) {
       Connection c = this.getConnection();
       String psql = "INSERT INTO SEC_ACC(CUSTOMERID, BALANCE, CURRENCY,OPENFEE, CLOSEFEE)"
             + "VALUES(?,?,?,?,?,?)";
       try {
          PreparedStatement pstmt = c.prepareStatement(psql, Statement.RETURN_GENERATED_KEYS);
-         pstmt.setString(1, secacc.getCustomerID());
-         pstmt.setDouble(2, secacc.getBalance());
-         pstmt.setString(3, secacc.getCurSig());
-         pstmt.setDouble(4, secacc.getOpenFee());
-         pstmt.setDouble(5, secacc.getCloseFee());
+         pstmt.setString(1, cid);
+         pstmt.setDouble(2, balance);
+         pstmt.setString(3, cursig);
+         pstmt.setDouble(4, openfee);
+         pstmt.setDouble(5, closefee);
          int result = pstmt.executeUpdate();
          System.out.println(result+" secacc created");
          pstmt.close();
@@ -226,14 +191,14 @@ public class DbHelperPSQL {
       return false;
    }
 
-   public boolean dropCustomer(Customer customer) {
+   public boolean dropCustomer(String username) {
       Connection c = this.getConnection();
       int result = 0;
       String psql = "DELETE FROM CUSTOMER WHERE USERNAME = ?";
       PreparedStatement pstmt;
       try {
          pstmt = c.prepareStatement(psql);
-         pstmt.setString(1, customer.getUname());
+         pstmt.setString(1, username);
          result = pstmt.executeUpdate();
          System.out.println(result+" customer dropped");
          c.commit();
@@ -297,16 +262,17 @@ public class DbHelperPSQL {
 
 
    public static void main(String args[]) throws SQLException {
-      String url = "jdbc:postgresql://localhost:5432/FancyBank";
-      String username = "postgres";
-      String pwd = null;
-      DbHelperPSQL dtbase = new DbHelperPSQL(url,username,pwd);
+      // String url = "jdbc:postgresql://localhost:5432/FancyBank";
+      // String username = "postgres";
+      // String pwd = null;
+      // DbHelperPSQL dtbase = new DbHelperPSQL(url,username,pwd);
+      DbHelperPSQL dtbase = new DbHelperPSQL();
       Customer c = new Customer("try2","test","test","test","123456","1@2","0000");
-      dtbase.insertCustomer(c);
+      dtbase.insertCustomer("try2","test","test","test","123456","1@2","0000");
       
       System.out.println("we have "+dtbase.getCusInfo(c));
       // Account a = 
-      dtbase.dropCustomer(c);
+      dtbase.dropCustomer("try2");
 
 
    }
